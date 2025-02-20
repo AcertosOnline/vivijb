df91137e41ca81442c5d1c9e2ddfd7816a7a3b4c
AcertosOnline <acertosonlinejogodobicho@gmail.com>
2025-02-20 08:59:45 -0300

    Add files via upload

---
+++ service-worker-vivijb.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'aguia-oficial-v1';
+const urlsToCache = [
+  '/',
+  '/ViviJB.html',
+  '/grupopalpites.png',
+  '/cassino.png',
+  '/manifest-vivijb.json'
+];
+
+// Instalação do Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Cache aberto');
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Ativação do Service Worker
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Removendo cache antigo:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Interceptação de requisições
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
